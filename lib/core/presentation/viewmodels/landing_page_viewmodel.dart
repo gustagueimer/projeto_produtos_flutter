f183a0bf662cf19ea9318b5567f78bb7c18fa763
gustagueimer <gustavomarqueslima@outlook.com>
2026-06-07 15:36:23 -0300

    fix: remoção temporária do state pra suprimir um erro

--- lib/core/presentation/viewmodels/landing_page_viewmodel.dart
+++ lib/core/presentation/viewmodels/landing_page_viewmodel.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:product_app/core/presentation/states/landing_page_state_provider.dart';
+//import 'package:product_app/core/presentation/states/landing_page_state_provider.dart';
 import 'package:product_app/features/product/presentation/pages/product_page.dart';
 import 'package:product_app/features/product/presentation/viewmodels/product_viewmodel.dart';
 
@@ -10,13 +10,11 @@ class LandingPageViewmodel {
   LandingPageViewmodel(this.ref);
 
   void naivgate(BuildContext context) {
-    ref.read(landingPageStateNotifierProvider.notifier).changeLoading();
     Navigator.push(
       context,
       MaterialPageRoute<void>(
         builder: (context) => ProductPage(viewModel: ProductViewModel(ref))
       )
     );
-    ref.read(landingPageStateNotifierProvider.notifier).changeLoading();
   }
 }
