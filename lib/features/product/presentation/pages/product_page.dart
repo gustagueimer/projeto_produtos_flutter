e424afb127582fd66f2ad214863565b0a9c440d8
gustagueimer <gustavomarqueslima@outlook.com>
2026-06-19 23:00:02 -0300

    feat: adição de botão para encerrar a sessão do usuário e voltar a tela de login, adição da imagem de perfil e nome do usuário logado no app bar

--- lib/features/product/presentation/pages/product_page.dart
+++ lib/features/product/presentation/pages/product_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:product_app/core/presentation/states/login_page_state_provider.dart';
 import 'package:product_app/features/product/presentation/states/product_state_provider.dart';
 import 'package:product_app/features/product/presentation/viewmodels/product_viewmodel.dart';
 
@@ -10,11 +11,33 @@ class ProductPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final plop = ref.watch(productStateNotifierProvider);
+    final plep = ref.read(loginPageStateNotifierProvider);
 
     return Scaffold(
-      appBar: AppBar(title: const Text("Products")),
+      appBar: AppBar(
+        title: const Text("Produtos"),
+        automaticallyImplyLeading: false,
+        actions: [
+          Row(
+            spacing: 8.0,
+            children: [
+              CircleAvatar(
+                radius: 20.0,
+                child: Image.network(plep.sessao?.image ?? "https://www.freeiconspng.com/thumbs/no-image-icon/no-image-icon-4.png"),
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(0, 0, 8.0, 0),
+                child:Text("${plep.sessao?.firstName} ${plep.sessao?.lastName}")
+              ),
+            ],
+          )
+        ],
+      ),
       body: Builder(
         builder: (context) {
+          if(plep.sessao == null) {
+            Navigator.maybePop(context);
+          }
           if(plop.isLoading == true) {
             return const Center(child: CircularProgressIndicator());
           }
@@ -52,12 +75,24 @@ class ProductPage extends ConsumerWidget {
         child: const Icon(Icons.download),
       ),
       persistentFooterButtons: [
-        Center(child: 
-          ElevatedButton(
-            onPressed: () {
-              viewModel.navigateToNewProduct(context);
-            }, 
-            child: Text("Criar Produto")
+        Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 12.0,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  viewModel.encerrarSessao();
+                }, 
+                child: Text("Encerrar Sessão")
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  viewModel.navigateToNewProduct(context);
+                }, 
+                child: Text("Criar Produto")
+              ),
+            ],
           ),
         ),
       ],
